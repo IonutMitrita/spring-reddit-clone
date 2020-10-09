d6d3273c90b0e3285740e8d15e5f05a9e8dc4694
IonutMitrita <mitrita.ionut@gmail.com>
2020-10-09 22:04:13 +0300

    Added controller and services

--- src/main/java/com/theminimalistdev/redditclone/config/SecurityConfig.java
+++ src/main/java/com/theminimalistdev/redditclone/config/SecurityConfig.java
@@ -1,4 +1,31 @@
 package com.theminimalistdev.redditclone.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //disabling csrf because csrf attacks can mainly occur when we are using
+        //session and cookies to authenticate the session info
+        //as rest apis are stateless by definition and since we are
+        //using json webtokens we can disable csrf protection
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
---
+++ src/main/java/com/theminimalistdev/redditclone/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.theminimalistdev.redditclone.controller;
+
+import com.theminimalistdev.redditclone.dto.RegisterRequest;
+import com.theminimalistdev.redditclone.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    AuthService authService;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful",
+                OK);
+    }
+}
